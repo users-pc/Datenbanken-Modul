4b1c94aef43f1b0c20fedc2cd604c05713a6ee16
rayan <rayan@Thinkpad-Rayan.speedport.ip>
2024-06-12 19:02:34 +0200

    Karten Übersicht und Aufteilung

--- Übung10/main/urls.py
+++ Übung10/main/urls.py
@@ -5,6 +5,11 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('hochschulen', views.hochschulen, name="hochschulen"),
     path('bevölkerung', views.bevölkerung, name="bevölkerung"),
+    #------------ Visualisierung durch Diagramme etc. ------------------
     path('visual', views.visual, name="visual"),
+    path('hochschulenvisual', views.hochschulenvisual, name="hochschulenvisual"),
+    # ------------ Karte ------------------
     path('karten', views.karten, name="karten"),
+    path('hochschulkarte', views.hochschulenmap, name="hochschulkarte"),
+    path('bevölerungkarte', views.bevölkerungsheatmap, name="bevölkerungskarte"),
 ]
--- Übung10/main/views.py
+++ Übung10/main/views.py
@@ -5,7 +5,8 @@ from .models import Hochschulen, Bevölkerung
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.contrib.postgres.search import SearchVector, SearchQuery
-
+import folium 
+from folium.plugins import MarkerCluster
 # Create your views here.
 def home(request):
     # einfügen der csv
@@ -144,18 +145,99 @@ def bevölkerung(request):
     page_obj = paginator.get_page(page_number)
     context = {'bevölkerung': page_obj, 'title': 'Bevölkerung'}
     return render(request, 'bevölkerung.html', context)
-
-
+# ------------------------------------------------------------------------------------------------------------------------------- Visualisierung/Diagramme
+# Zur Navigation
 def visual(request):
+    return render(request, 'visual.html')
+
+def hochschulenvisual(request):
     hochschulen = Hochschulen.objects.all()
-    bevölkerung = Bevölkerung.objects.all()
     hopenclosed_plotly=hopenclosed(hochschulen)
     uni_vs_fachhochschule_plotly=uni_vs_fachhochschule(hochschulen)
     studis_an_uni_vs_fachhochschule_plotly=studis_an_uni_vs_fachhochschule(hochschulen)
     studis_bundesland_plotly=studis_bundesland(hochschulen)
     context = {'hopenclosed': hopenclosed_plotly,'uni_vs_fachhochschule': uni_vs_fachhochschule_plotly, 'studis_an_uni_vs_fachhochschule': studis_an_uni_vs_fachhochschule_plotly,'title': 'Visualisierung', 'studis_bundesland': studis_bundesland_plotly}
-    return render(request, 'visual.html', context)
+    return render(request, 'hochschulenvisual.html', context)
 
 
+
+
+
+# ------------------------------------------------------------------------------------------------------------------------------- Karten 
+# Zur Navigation
 def karten(request):
     return render(request, 'karten.html')
+
+def hochschulenmap(request):
+     # Karte von Folium mit allen hochschulen in deutschland
+    m = folium.Map(width='100%',height='70%',location=[51.165707, 10.452764], zoom_start=5, name="Inserate")    
+    map_cluster = MarkerCluster().add_to(m)
+    for hochschule in Hochschulen.objects.all():
+        html = f'<a href="{hochschule.homepage}"> {hochschule.name} </a>'
+        folium.Marker(
+            location=[hochschule.latitude, hochschule.longitude],
+            popup=html, # Im Popup kann ein a href zu der homepage der Hochschule eingefügt werden
+
+            icon=folium.Icon(color='blue')
+        ).add_to(map_cluster)
+
+
+    context = {'map': m._repr_html_(), 'title': 'Karte'}
+    return render(request, 'hochschulenmap.html', context)
+
+import pandas as pd
+import requests
+import json
+
+def bevölkerungsheatmap(request):
+    # Mache eine choropleth map mit der Bevölkerungsdichte in Deutschland mit Folium 
+    # Die Daten für die Bevölkerungsdichte sind in der Bevölkerungstabelle gespeichert
+    geo_data='https://raw.githubusercontent.com/isellsoap/deutschlandGeoJSON/master/2_bundeslaender/4_niedrig.geo.json',
+    # geojson importieren und file herunterladen
+
+    geojson_data_install = requests.get(geo_data)
+    with open('geojson_data.json', 'wb') as f:
+        f.write(geojson_data_install.content)
+    
+
+    population = Bevölkerung.objects.all()
+    bundesländer = ['Baden-Württemberg', 'Bayern', 'Berlin', 'Brandenburg', 'Bremen', 'Hamburg', 'Hessen', 'Mecklenburg-Vorpommern', 'Niedersachsen', 'Nordrhein-Westfalen', 'Rheinland-Pfalz', 'Saarland', 'Sachsen', 'Sachsen-Anhalt', 'Schleswig-Holstein', 'Thüringen']
+    # Filtere die Daten für die Bundesländer
+    for i in population:
+        # Koordinaten für die Orte werden benötigt schauen, ob es im Bundesland liegt
+        from .views_helper import haversine
+        koordinaten = [i.latitude, i.longitude]
+        # jetzt soll unter den geojson_data_install die Koordinaten der Orte mit den Koordinaten der Bundesländer verglichen werden
+        for bundesland in bundesländer:
+            # Koordinaten der Bundesländer
+            bundesland_koordinaten = []
+            with open('geojson_data.json') as f:
+                data = json.load(f)
+                for feature in data['features']:
+                    if feature['properties']['name'] == bundesland:
+                        bundesland_koordinaten = feature['geometry']['coordinates']
+                        break
+            # Jetzt haben wir die Koordinaten der Bundesländer und die Koordinaten der Orte
+            # Jetzt schauen wir, ob die Koordinaten der Orte in den Koordinaten der Bundesländer liegen
+            if haversine(koordinaten, bundesland_koordinaten) == True:
+                print(i.ort, "liegt in", bundesland)
+                i.bundesland = bundesland
+                i.save()
+
+    # Population in Deutschland als pd.DataFrame
+    population_df = pd.DataFrame.from_records(population.values())
+    m = folium.Map(width='100%',height='70%',location=[51.165707, 10.452764], zoom_start=5, name="Inserate")
+    # Lade die GeoJson Datei für die Bundesländer
+    folium.Choropleth(
+        geo_data='https://raw.githubusercontent.com/isellsoap/deutschlandGeoJSON/master/2_bundeslaender/4_niedrig.geo.json',
+        name='choropleth',
+        data=population_df,
+        # Spalte mit der Bevölkerungsdichte und den Koordinaten der Orte
+        columns=['ort', 'total_population'],
+        key_on='feature.properties.name',
+        fill_opacity=0.7,
+        line_opacity=0.2,
+        legend_name='Bevölkerungsdichte'
+    ).add_to(m)
+    context = {'map': m._repr_html_(), 'title': 'Karte'}
+    return render(request, 'bevölkerungsheatmap.html', context)
